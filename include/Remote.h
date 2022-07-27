65fc20cf051cb8790d85d7ba4a242cf627eb5abd
jman168 <jasonabeckmann@gmail.com>
2022-07-26 21:56:15 -0400

    add nrf24 support for remote (#1)

---
+++ include/Remote.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <SPI.h>
+#include "RF24.h"
+
+struct AKCPacket {
+    float speed;
+    float battery;
+};
+
+struct ControlPacket {
+    float throttle;
+};
+
+class Remote {
+    public:
+        Remote();
+
+        void handle();
+
+        float getThrottle();
+        void setBattery(float battery);
+        void setSpeed(float speed);
+
+    private:
+        RF24 *_radio;
+
+        float _throttle;
+        float _battery = 0.0;
+        float _speed = 0.0;
+
+        const char _boardAddr[6] = "BESK8"; // board address
+        const char _remoteAddr[6] = "RESK8"; // remote address
+
+    private:
+        void writeAckPacket();
+};
