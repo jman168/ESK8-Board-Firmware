0d216723fcd541b77ae6bdfcf1253ace7a3ddc29
Jason <jasonabeckmann@gmail.com>
2022-07-30 23:02:10 -0400

    functional remote interface

--- src/Remote.cpp
+++ src/Remote.cpp
@@ -1,56 +1,76 @@
 #include "Remote.h"
 
-Remote::Remote() {
-    _radio = new RF24(22, 21);
+struct remote_ack_packet_t {
+    float speed;
+    float battery;
+};
 
-    if(!_radio->begin()) {
+struct remote_controller_packet_t {
+    float throttle;
+};
+
+RF24 *remote_radio;
+
+float remote_throttle;
+float remote_battery = 0.0;
+float remote_speed = 0.0;
+unsigned long remote_last_packet = 0;
+
+const char remote_board_addr[6] = "BESK8"; // board address
+const char remote_remote_addr[6] = "RESK8"; // remote address
+
+void remote_write_ack_packet() {
+    remote_ack_packet_t ack_packet;
+    ack_packet.battery = remote_battery;
+    ack_packet.speed = remote_speed;
+    remote_radio->writeAckPayload(1, &ack_packet, sizeof(ack_packet));
+}
+
+void remote_init() {
+    remote_radio = new RF24(22, 21);
+
+    if(!remote_radio->begin()) {
         Serial.println("Radio hardware is not responding!!");
+        while(true) {}
     }
 
-    _radio->setPALevel(RF24_PA_HIGH);
+    remote_radio->setPALevel(RF24_PA_HIGH);
 
-    _radio->enableDynamicPayloads();
+    remote_radio->enableDynamicPayloads();
 
-    _radio->enableAckPayload();
+    remote_radio->enableAckPayload();
 
-    _radio->openWritingPipe((const uint8_t*)&_remoteAddr);
-    _radio->openReadingPipe(1, (const uint8_t*)&_boardAddr);
+    remote_radio->openWritingPipe((const uint8_t*)&remote_remote_addr);
+    remote_radio->openReadingPipe(1, (const uint8_t*)&remote_board_addr);
 
-    writeAckPacket();
+    remote_write_ack_packet();
 
-    _radio->startListening();
+    remote_radio->startListening();
 }
 
-void Remote::handle() {
+void remote_update() {
     uint8_t pipe;
-    if (_radio->available(&pipe)) {
-        lastPacket = millis();
-        ControlPacket packet;
-        _radio->read(&packet, sizeof(packet));
-        _throttle = packet.throttle;
-        writeAckPacket();
+    if (remote_radio->available(&pipe)) {
+        remote_last_packet = millis();
+        remote_controller_packet_t packet;
+        remote_radio->read(&packet, sizeof(packet));
+        remote_throttle = packet.throttle;
+        remote_write_ack_packet();
     }
 }
 
-float Remote::getThrottle() {
-    return _throttle;
+float remote_get_throttle() {
+    return remote_throttle;
 }
 
-void Remote::setBattery(float battery) {
-    _battery = battery;
+void remote_set_battery(float battery) {
+    remote_battery = battery;
 }
 
-void Remote::setSpeed(float speed) {
-    _speed = speed;
+void remote_set_speed(float speed) {
+    remote_speed = speed;
 }
 
-bool Remote::isConnected() {
-    return !((millis() - lastPacket) >= 100);
-}
-
-void Remote::writeAckPacket() {
-    AKCPacket ackPacket;
-    ackPacket.battery = _battery;
-    ackPacket.speed = _speed;
-    _radio->writeAckPayload(1, &ackPacket, sizeof(ackPacket));
+bool remote_is_connected() {
+    return !((millis() - remote_last_packet) >= 100);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,23 @@
 #include "MotorController.h"
 #include "Battery.h"
 
-Remote *remote;
-
 void setup() {
   Serial.begin(115200);
 
   battery_init();
   encoder_init();
   motor_controller_init();
-
-  remote = new Remote();
+  remote_init();
 }
 
 void loop() {
-  remote->handle();
+  remote_update();
   
-  motor_controller_set_throttle(remote->getThrottle());
+  motor_controller_set_throttle(remote_get_throttle());
   motor_controller_update();
 
-  Serial.println(remote->isConnected());
+  Serial.println(remote_is_connected());
 
-  remote->setSpeed(encoder_get_speed_mph());
-  remote->setBattery(battery_get_charge());
+  remote_set_speed(encoder_get_speed_mph());
+  remote_set_battery(battery_get_charge());
 }
