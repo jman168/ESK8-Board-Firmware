403dec98360c3f4124aae7e68cf302fae9d93021
jman168 <jasonabeckmann@gmail.com>
2022-08-08 19:21:04 -0400

    Can spark (#18)
    * add can spark interface
    
    * tuned motor KV
    
    * upped max speed to 10mph

--- src/MotorController.cpp
+++ src/MotorController.cpp
@@ -1,7 +1,9 @@
 #include "MotorController.h"
 
-Servo motor_controller_motor;
+SparkMax *motor_controller_spark;
+PIDController pidController(0.0, 0.4, 0.0, MAX_VOLTAGE);
 
+float motor_controller_output_voltage = 0.0;
 float motor_controller_throttle = 0.0;
 bool motor_controller_stop = true;
 
@@ -9,47 +11,75 @@ float clamp(float input, float minV = -1.0, float maxV = 1.0) {
     return min(max(input, minV), maxV);
 }
 
-float motor_controller_mps_to_voltage(float mps) {
-    return mps/MOTOR_KV;
+void motor_controller_init(SparkMax *spark) {
+    motor_controller_spark = spark;
 }
 
-void motor_controller_init() {
-    motor_controller_motor.attach(MOTOR_PIN);
-}
+// void motor_controller_update() {
+//     if(motor_controller_stop) {
+//         if(abs(motor_controller_spark->getVelocity()) > STOPPED_THRESHOLD) { // if the board is not stopped try to stop it
+//             if(motor_controller_spark->getVelocity() > 0.0) {
+//                 motor_controller_set_current(-BREAKING_CURRENT);
+//                 motor_controller_set_voltage(pidController.compute(motor_controller_get_signed_current()));
+//             }
+
+//             if(motor_controller_spark->getVelocity() < 0.0) {
+//                 motor_controller_set_current(BREAKING_CURRENT);
+//                 motor_controller_set_voltage(pidController.compute(motor_controller_get_signed_current()));
+//             }
+//         }   
+
+//         else { // if the board is stopped apply 0 output.
+//             motor_controller_set_voltage(0.0);
+//         }
+//     }
+    
+//     else { // only to run of the board is not commanded to stop.
+//         motor_controller_set_voltage(pidController.compute(motor_controller_get_signed_current()));
+//     }
+// }
+
+// void motor_controller_update() {
+//     float current = motor_controller_get_signed_current();
+//     float pidOutput = pidController.compute(current);
+
+//     motor_controller_set_voltage(pidOutput);
+// }
 
 void motor_controller_update() {
-    if(motor_controller_stop) {
-        if(abs(encoder_get_speed_mps()) > STOPPED_THRESHOLD) { // if the board is not stopped try to stop it
-            if(encoder_get_speed_mps() > 0.0) {
-                motor_controller_set_voltage(clamp(motor_controller_mps_to_voltage(encoder_get_speed_mps()) - STOP_VOLTAGE, -MAX_VOLTAGE, MAX_VOLTAGE));
-            }
+    float current = motor_controller_get_signed_current();
+    float pidOutput = pidController.compute(current);
 
-            if(encoder_get_speed_mps() < 0.0) {
-                motor_controller_set_voltage(clamp(motor_controller_mps_to_voltage(encoder_get_speed_mps()) + STOP_VOLTAGE, -MAX_VOLTAGE, MAX_VOLTAGE));
-            }
-        }   
-
-        else { // if the board is stopped apply 0 output.
-            motor_controller_set_voltage(0.0);
-        }
+    if(motor_controller_throttle == 0.0 && abs(motor_controller_spark->getVelocity()) < STOPPED_THRESHOLD) {
+        motor_controller_set_voltage(0.0);
     }
-    
-    else { // only to run of the board is not commanded to stop.
-        motor_controller_set_voltage(clamp(motor_controller_mps_to_voltage(encoder_get_speed_mps()) + motor_controller_throttle*MAX_DELTA_V, -MAX_VOLTAGE, MAX_VOLTAGE));
+
+    else {
+        motor_controller_set_voltage(pidOutput);
     }
 }
 
 void motor_controller_set_voltage(float voltage) {
-    voltage = clamp(voltage, -MAX_VOLTAGE, MAX_VOLTAGE);
-    
-    float power = ( voltage/BATTERY_VOLTAGE + 1.0 ) / 2.0;
-    int MS = MIN_MS + (MAX_MS-MIN_MS) * power;
-
-    motor_controller_motor.writeMicroseconds(MS);
+    motor_controller_output_voltage = clamp(voltage, -MAX_VOLTAGE, MAX_VOLTAGE);
+    motor_controller_spark->setVoltage(
+        clamp(voltage, -MAX_VOLTAGE, MAX_VOLTAGE)
+    );
 }
 
 void motor_controller_set_throttle(float throttle) {
-    motor_controller_throttle = clamp(throttle);
+    motor_controller_throttle = throttle;
+    pidController.set(throttle*MAX_CURRENT);
+}
+
+float motor_controller_get_signed_current() {
+    float backEMF = motor_controller_spark->getVelocity() / MOTOR_KV;
+    float voltageDiff = motor_controller_output_voltage - backEMF;
+
+    if(voltageDiff < 0.0) {
+        return -motor_controller_spark->getOutputCurrent();
+    } else {
+        return motor_controller_spark->getOutputCurrent();
+    }
 }
 
 void motor_controller_set_stop(bool stop) {
